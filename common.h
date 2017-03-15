a179fbb9a00d8ab211a2a381f7a946bfde0cfb5d
lanbinchang <lanbin0303@gmail.com>
2017-03-15 13:13:57 +0800

    add common.h

---
+++ common.h
@@ -0,0 +1,15 @@
+/**
+ * author:lanbin0303@gmail.com
+ *
+ * Features:
+ * 1.define macros and ariables
+ * 2.include common head files
+ */
+#ifndef __Common_H__
+#define __Common_H__
+#include <vector>
+#include <string>
+#include <map>
+#include <iostream>
+using namespace std;
+#endif
