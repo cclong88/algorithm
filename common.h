97ae1e78a2ae3744adedc203b6a8d5013b7ee7cb
lanbinchang <lanbin0303@gmail.com>
2017-03-15 13:41:34 +0800

    modify common.h,add tool.h

--- common.h
+++ common.h
@@ -4,6 +4,7 @@
  * Features:
  * 1.define macros and ariables
  * 2.include common head files
+ * 3.basic storage structure
  */
 #ifndef __Common_H__
 #define __Common_H__
@@ -12,4 +13,25 @@
 #include <map>
 #include <iostream>
 using namespace std;
+
+//singly linked list node
+struct ListNode
+{
+    int value;
+    ListNode *next;
+};
+//doubly linked list node
+struct DoubleListNode
+{
+    int value;
+    DoubleListNode *pre;
+    DoubleListNode *next;
+};
+//binary tree node
+struct TreeNode
+{
+    int value;
+    TreeNode *left;
+    TreeNode *right;
+};
 #endif
